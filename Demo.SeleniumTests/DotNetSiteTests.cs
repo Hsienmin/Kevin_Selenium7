f32a4ad91b7692a24b4efa67b53c17334e503233
Kevin Yang <kyang@altigen.com>
2019-10-03 15:37:24 +0800

    1st Initial

--- Demo.SeleniumTests/DotNetSiteTests.cs
+++ Demo.SeleniumTests/DotNetSiteTests.cs
@@ -1,7 +1,7 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System; 
+using System;
 using System.Linq;
 
 namespace Demo.SeleniumTests
@@ -9,41 +9,37 @@ namespace Demo.SeleniumTests
     [TestClass]
     public class DotNetSiteTests
     {
+        public TestContext TestContext { get; set; }
+
         [TestMethod]
         public void TestGetStarted()
         {
             // Chrome Driver was manually downloaded from https://sites.google.com/a/chromium.org/chromedriver/downloads
-            // parameter "." will instruct to look for the chromedriver.exe in the current folder
+            // parameter "." will instruct to look for the chromedriver.exe in the current folder (bin/debug/...)
 
-            //Run Google Chrome Driver with UI Visual
+            // Run Selenium in UI visiable mode
             //using (var driver = new ChromeDriver("."))
 
-            var options = new ChromeOptions();
-            options.AddArgument("headless");
-            using (var driver = new ChromeDriver(".", options))            
-                {
-
+            // Run Selenium in UI headless mode
+             var options = new ChromeOptions();
+             options.AddArgument("headless");
+             using (var driver = new ChromeDriver(".", options))
+            {
+                //Directly navgate the URL
                 driver.Navigate().GoToUrl("http://teamsportaldemo.azurewebsites.net/");
+
+                //Navgate the URL form the "webAppUrl" variable from the ".runsettings" file. 
+                //driver.Navigate().GoToUrl((string)TestContext.Properties["webAppUrl"]);
+
+                //Put Selenium code here.
                 driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(12);
                 driver.FindElement(By.Name("email")).SendKeys("admin@altigen.com");
                 driver.FindElement(By.Name("password")).SendKeys("222222");
                 driver.FindElement(By.ClassName("MuiButton-label")).Click();
 
-                // Get Started section is a multi-step wizard
-                // The following sections will find the visible next step button until there's no next step button left
-                //IWebElement nextLink = null;
-                //do
-                //{
-                //    nextLink?.Click();
-                //    nextLink = driver.FindElements(By.CssSelector(".step:not([style='display:none;']):not([style='display: none;']) .step-select")).FirstOrDefault();
-                //} while (nextLink != null);
-
-                // on the last step, grab the title of the step wich should be equal to "Next steps"
-                //var lastStepTitle = driver.FindElement(By.CssSelector(".step:not([style='display:none;']):not([style='display: none;']) h2")).Text;
-
-                // verify the title is the expected value "Next steps"
-                // Assert.AreEqual(lastStepTitle, "Next steps");
             }
         }
+
+  
     }
 }
